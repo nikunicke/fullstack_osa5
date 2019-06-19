c1c7d867d386fb17cce7405a02ad805527bb739e
nikunicke <pimenoff.niklas@gmail.com>
2019-06-19 20:27:24 +0300

    formatting fix

---
+++ bloglist-frontend/src/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import notificationReducer from './reducers/notificationReducer'
+
+// Import redcers
+
+const reducer = combineReducers({
+    notification: notificationReducer
+})
+
+const store = createStore(
+    reducer,
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
+)
+
+export default store
